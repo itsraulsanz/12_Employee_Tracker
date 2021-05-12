a26a9005bb15ce9f83e6fdaa52b38113233c0d59
Raul Sanz <luar_znas@hotmail.com>
2021-05-12 14:05:59 +0100

    JS: Creating the prompts and the different functions to run

--- employeeTracker.js
+++ employeeTracker.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql');
+const inquirer = require('inquirer');
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -17,4 +18,58 @@ const connection = mysql.createConnection({
 connection.connect((err) => {
   if (err) throw err;
   console.log(`connected as id ${connection.threadId}`);
+  runPrompt();
 });
+
+const runPrompt = () => {
+  inquirer
+    .prompt({
+      name: 'action',
+      type: 'rawlist',
+      message: 'What would you like to do?',
+      choices: [
+        'View All Employees By Department',
+        'View All Employees By Manager',
+        'Add Employee',
+        'Remove Employee',
+        'Update Employee Role',
+        'Update Employee Manager',
+        'View All Roles',
+      ],
+    })
+    .then((answer) => {
+      switch (answer.action) {
+        case 'View All Employees By Department':
+          employeesByDepartmentSearch();
+          break;
+
+        case 'View All Employees By Manager':
+          employeesByManagerSearch();
+          break;
+
+        case 'Add Employee':
+          addEmployee();
+          break;
+
+        case 'Remove Employee':
+          removeEmployee();
+          break;
+
+        case 'Update Employee Role':
+          updateEmployeeRole();
+          break;
+        
+        case 'Update Employee Manager':
+          updateEmployeManager();
+          break;
+
+        case 'View All Roles':
+            rolesSearch();
+            break;  
+
+        default:
+          console.log(`Invalid action: ${answer.action}`);
+          break;
+      }
+    });
+};
